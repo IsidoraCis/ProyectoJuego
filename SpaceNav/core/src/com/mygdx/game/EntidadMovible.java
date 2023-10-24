38323d707b48396a6a37c5cfb22c96a4a3f562bf
Rodolfo Fernández <rodolfo.fernandez.v@mail.pucv.cl>
2023-10-24 12:02:40 -0300

    Creacion clase abstracta EntidadMovible

--- SpaceNav/core/src/com/mygdx/game/EntidadMovible.java
+++ SpaceNav/core/src/com/mygdx/game/EntidadMovible.java
@@ -1,5 +1,51 @@
 package com.mygdx.game;
 
 public abstract class EntidadMovible {
+    protected float x;
+    protected float y;
+    protected float xSpeed;
+    protected float ySpeed;
 
+    public EntidadMovible(float x, float y, float xSpeed, float ySpeed) {
+        this.x = x;
+        this.y = y;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public abstract void update();
+
+    // Getters y Setters para los atributos
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getXSpeed() {
+        return xSpeed;
+    }
+
+    public void setXSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public float getYSpeed() {
+        return ySpeed;
+    }
+
+    public void setYSpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+}
 }
