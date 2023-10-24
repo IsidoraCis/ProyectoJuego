026d4f023e4b10174af22ed978214310397fea5e
Rodolfo Fernández <rodolfo.fernandez.v@mail.pucv.cl>
2023-10-24 12:07:31 -0300

    Modificacion clase bullet
    Extendimos e implementamos la clase abstracta y la interfaz correspondiente a esta clase.

--- SpaceNav/core/src/com/mygdx/game/Bullet.java
+++ SpaceNav/core/src/com/mygdx/game/Bullet.java
@@ -6,44 +6,41 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 
-public class Bullet {
-
-	private int xSpeed;
-	private int ySpeed;
+public class Bullet extends EntidadMovible implements Dibujable{
 	private boolean destroyed = false;
 	private Sprite spr;
-	    
-	    public Bullet(float x, float y, int xSpeed, int ySpeed, Texture tx) {
-	    	spr = new Sprite(tx);
-	    	spr.setPosition(x, y);
-	        this.xSpeed = xSpeed;
-	        this.ySpeed = ySpeed;
-	    }
-	    public void update() {
-	        spr.setPosition(spr.getX()+xSpeed, spr.getY()+ySpeed);
-	        if (spr.getX() < 0 || spr.getX()+spr.getWidth() > Gdx.graphics.getWidth()) {
-	            destroyed = true;
-	        }
-	        if (spr.getY() < 0 || spr.getY()+spr.getHeight() > Gdx.graphics.getHeight()) {
-	        	destroyed = true;
-	        }
-	        
-	    }
-	    
-	    public void draw(SpriteBatch batch) {
-	    	spr.draw(batch);
-	    }
-	    
-	    public boolean checkCollision(Ball2 b2) {
-	        if(spr.getBoundingRectangle().overlaps(b2.getArea())){
-	        	// Se destruyen ambos
-	            this.destroyed = true;
-	            return true;
-	
-	        }
-	        return false;
-	    }
-	    
-	    public boolean isDestroyed() {return destroyed;}
-	
+
+	public Bullet(float x, float y, int xSpeed, int ySpeed, Texture tx) {
+		super(x, y, xSpeed, ySpeed);
+		spr = new Sprite(tx);
+		spr.setPosition(getX(), getY());
+	}
+	//Comprueba colisiones con el borde de la pantalla
+	public void update() {
+		spr.setPosition(spr.getX()+xSpeed, spr.getY()+ySpeed);
+		if (spr.getX() < 0 || spr.getX()+spr.getWidth() > Gdx.graphics.getWidth()) {
+			destroyed = true;
+		}
+		if (spr.getY() < 0 || spr.getY()+spr.getHeight() > Gdx.graphics.getHeight()) {
+			destroyed = true;
+		}
+
+	}
+
+	public void draw(SpriteBatch batch) {
+		spr.draw(batch);
+	}
+	//Comprueba colision con las pelotas
+	public boolean checkCollision(Ball2 b2) {
+		if(spr.getBoundingRectangle().overlaps(b2.getArea())){
+			// Se destruyen ambos
+			this.destroyed = true;
+			return true;
+
+		}
+		return false;
+	}
+
+	public boolean isDestroyed() {return destroyed;}
+
 }
